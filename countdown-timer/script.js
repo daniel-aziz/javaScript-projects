9391f344a34f29584143ae9eccc26898108bd9a2
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-03-22 22:43:18 +0200

    countdown prj finished

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,12 +1,54 @@
-const newYears = '01.01.2023';
+
+// Elements
+const daysElem = document.getElementById('days');
+const hoursElem = document.getElementById('hours');
+const minElem = document.getElementById('minutes');
+const secElem = document.getElementById('seconds');
+const titleElem = document.getElementById('event-title');
+
+var initDate = '01.01.2023';
+var initTitle = titleElem.innerText;
+
+console.log(initDate)
+console.log(initTitle)
+
+function submit() {
+    
+    //TITLE 
+    const userTitle = document.getElementById('input-title').value;
+    console.log(userTitle)
+    titleElem.innerText = (userTitle !== undefined || userTitle !== "" )? initTitle : userTitle;
+
+
+    // DATE
+    const userDate = document.getElementById('input-date').value;
+    console.log(userDate)
+    initDate = (userDate === undefined || userDate === "")? initDate : userDate;
+
+    countDown();
+}
+
 
 function countDown() {
-    const newYearsDate = new Date(newYears);
+
+    const desiredDate = new Date(initDate);
     const currentDate = new Date();
 
-    const seconds = (newYearsDate - currentDate) / 1000;
-    const days = Math.floor(seconds / 3600 / 24 / 24)
-    console.log(days)
+    const secDiff = (desiredDate - currentDate) / 1000;
+
+    const days = Math.floor(secDiff / 3600 / 24);
+    const hours = Math.floor(secDiff / 3600) % 24;
+    const minutes = Math.floor(secDiff / 60) % 60;
+    const seconds = Math.floor(secDiff) % 60;
+
+    daysElem.innerText = days;
+    hoursElem.innerText = formatTime(hours);
+    minElem.innerText = formatTime(minutes);
+    secElem.innerText = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
 }
 
 countDown();
