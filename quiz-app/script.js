b3a5289cddbc2397df2845929fafe087fe109b4a
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-03-23 14:00:33 +0200

    added and updated README

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,19 +1,19 @@
 const quizData = [
     {
-        question: "What is the most used programing language used in 19`?",
+        question: "What is the most used programming language in 2021?",
         a: "Java",
-        b: "C",
+        b: "C#",
         c: "Python",
         d: "JavaScript",
-        correct: "d"
+        correct: "c",
     },
     {
-        question: "Who is the President of USA?",
-        a: "Donald Trump",
-        b: "Barak Obama",
-        c: "George Bush",
+        question: "Who is the President of the USA?",
+        a: "Gorge Bush",
+        b: "Donald Trump",
+        c: "Barak Obama",
         d: "Joe Biden",
-        correct: "d"
+        correct: "d",
     },
     {
         question: "What does HTML stand for?",
@@ -30,9 +30,18 @@ const quizData = [
         c: "1994",
         d: "none of the above",
         correct: "b",
-    }
-]
+    },
+    {
+        question: "How many continents are there?",
+        a: "4",
+        b: "7",
+        c: "5",
+        d: "We are all connected",
+        correct: "b",
+    },
+];
 
+const quiz = document.getElementById('quiz');
 const questionElem = document.getElementById('question')
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -40,14 +49,14 @@ const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit')
 
-
-console.table(quizData)
-
 let currentQuestionIndex = 0;
+let score = 0;
+
 loadQuiz();
 
 function loadQuiz() {
-    const currentQuestion = quizData[currentQuestionIndex]
+    deselectAnswers();
+    const currentQuestion = quizData[currentQuestionIndex];
     questionElem.innerText = currentQuestion.question;
     a_text.innerText = currentQuestion.a;
     b_text.innerText = currentQuestion.b;
@@ -55,12 +64,57 @@ function loadQuiz() {
     d_text.innerText = currentQuestion.d;
 }
 
+function getSelected() {
+    const answersElem = document.querySelectorAll('.answer');
+
+    let answer = undefined;
+
+    answersElem.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    const answersElem = document.querySelectorAll('.answer');
+    answersElem.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
 submitBtn.addEventListener('click', () => {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < quizData.length) loadQuiz();
-    else { 
-        // TODO: Show Resultes. 
-        alert('You Finished!') 
+    const userAnswer = getSelected();
+
+    if (userAnswer) {
+        if (userAnswer === quizData[currentQuestionIndex].correct) {
+            score++;
+        }
+
+        currentQuestionIndex++;
+        
+        if (currentQuestionIndex < quizData.length) {
+            loadQuiz();
+        }
+        else {
+            quiz.innerHTML = `
+            <h2>
+            You answered correctly at ${score}/${quizData.length} 
+            questions!  
+            </h2>
+            `
+            submitBtn.innerHTML = `
+            <button onclick="location.reload()">Reload</button>
+            `
+        }
     }
+    else {
+        
+    }
+
+
+
 })
 
